b123c2925c5db6faa5ea4ea8839d56f2446465d6
Steven Haryanto <stevenharyanto@gmail.com>
2013-11-05 16:33:52 +0000

    Import of SHARYANTO/Path-Naive-0.02 from CPAN.
    gitpan-cpan-distribution: Path-Naive
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         SHARYANTO/Path-Naive-0.02.tar.gz
    gitpan-cpan-author:       SHARYANTO
    gitpan-cpan-maturity:     released

--- lib/Path/Naive.pm
+++ lib/Path/Naive.pm
@@ -4,7 +4,7 @@ use 5.010001;
 use strict;
 use warnings;
 
-our $VERSION = '0.01'; # VERSION
+our $VERSION = '0.02'; # VERSION
 
 use Exporter;
 our @ISA = qw(Exporter);
@@ -50,10 +50,19 @@ sub is_rel_path {
 }
 
 sub concat_path {
-    die "Please specify path1" unless defined($_[0]) && length($_[0]);
-    die "Please specify path2" unless defined($_[1]) && length($_[1]);
-    return $_[1] if $_[1] =~ m!\A/!;
-    $_[0] . ($_[0] =~ m!/\z! ? "" : "/") . $_[1];
+    die "Please specify at least two paths" unless @_ > 1;
+    my $i = 0;
+    my $res = $_[0];
+    for (@_) {
+        die "Please specify path (#$i)" unless defined && length;
+        next unless $i++;
+        if (m!\A/!) {
+            $res = $_;
+        } else {
+            $res .= ($res =~ m!/\z! ? "" : "/") . $_;
+        }
+    }
+    $res;
 }
 
 sub concat_path_n {
@@ -82,7 +91,7 @@ Path::Naive - Yet another abstract, Unix-like path manipulation routines
 
 =head1 VERSION
 
-version 0.01
+version 0.02
 
 =head1 SYNOPSIS
 
@@ -175,9 +184,9 @@ are used: Config::Tree, L<Riap> (L<App::riap>).
 
 =head2 is_rel_path($path) => BOOL
 
-=head2 concat_path($path1, $path2) => STR
+=head2 concat_path($path1, $path2, ...) => STR
 
-=head2 concat_path_n($path1, $path2) => STR
+=head2 concat_path_n($path1, $path2, ...) => STR
 
 =head2 abs_path($path) => STR
 
